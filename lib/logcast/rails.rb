4555dbb753b08cf5a6ac55ef234f186f6a4598f7
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-12-31 16:35:13 -0800

    don't use alias_method_chain

--- lib/logcast/rails.rb
+++ lib/logcast/rails.rb
@@ -3,14 +3,12 @@ require 'logcast'
 module Logcast
   class Railtie < Rails::Railtie
     initializer 'logcast.initialize', :before => :initialize_logger do |app|
-      class << Rails
-        def logger_with_logcast=(logger)
-          broadcast = Logcast::Broadcaster.new
-          broadcast.subscribe(logger)
-          self.logger_without_broadcaster = broadcast
-        end
+      original = Rails.method(:logger=)
 
-        alias_method_chain :logger=, :logcast
+      Rails.define_singleton_method(:logger=) do |logger|
+        broadcast = Logcast::Broadcaster.new
+        broadcast.subscribe(logger)
+        original.call(broadcast)
       end
     end
   end
