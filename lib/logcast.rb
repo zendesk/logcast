8ff721c85231fd0e6b93ffede9793c230bf799b1
Steven Davidovitz <sdavidovitz@zendesk.com>
2013-12-31 16:30:31 -0800

    turn logcast/rails into a railtie that overrides Rails.logger=

--- lib/logcast.rb
+++ lib/logcast.rb
@@ -1,9 +1,3 @@
 module Logcast
   autoload :Broadcaster, 'logcast/broadcaster.rb'
-
-  def self.engage!
-    logger = Rails.logger
-    Rails.logger = Logcast::Broadcaster.new
-    Rails.logger.subscribe(logger)
-  end
 end
--- lib/logcast/rails.rb
+++ lib/logcast/rails.rb
@@ -1,3 +1,17 @@
 require 'logcast'
 
-Logcast.engage!
+module Logcast
+  class Railtie < Rails::Railtie
+    initializer 'logcast.initialize', :before => :initialize_logger do |app|
+      class << Rails
+        def logger_with_logcast=(logger)
+          broadcast = Logcast::Broadcaster.new
+          broadcast.subscribe(logger)
+          self.logger_without_broadcaster = broadcast
+        end
+
+        alias_method_chain :logger=, :logcast
+      end
+    end
+  end
+end
